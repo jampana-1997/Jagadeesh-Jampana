cd44b8a2df6bf39c913d3ffafb7c575d1d1d39d9
Jagadeesh Jampana <jampana633@gmail.com>
2026-06-11 17:20:58 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const CSV_URL =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vRIwfuJU43o5Ee3hlt_3EmXzOu0rsTIzJKxkCEAuwDNmmhshgeBMZFoEp9YPZYnkTEzUoQ8BEBx93yq/pub?gid=1181255206&single=true&output=csv";
+window.onload=function(){
+    loadData();
+}
